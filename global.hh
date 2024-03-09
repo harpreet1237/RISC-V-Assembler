017d334e017e6077aa4c7f145ea27a4bbf06cac2
Harpreet Singh <harpreetsingh.btd20@gmail.com>
2024-03-09 23:35:26 +0530

    global-initialization-opcodes-done

--- global.hh
+++ global.hh
@@ -7,7 +7,8 @@ uint32_t Instruction_Address = 0;
 
 map<string,uint32_t>opcode;
 map<string,string>format_code;
-
+map<std::string, uint32_t> func3;
+map<std::string, uint32_t> func7;
 
 vector<string> R_Format = {"add", "and", "or", "sll", "slt", "sra", "srl", "sub", "xor", "mul", "div", "rem"};
 vector<string> I_Format = {"addi", "andi", "ori", "lb", "ld", "lh", "lw", "jalr","slli","srai"};
@@ -15,6 +16,139 @@ vector<string> S_Format = {"sb", "sw", "sd", "sh"};
 vector<string> SB_Format = {"beq", "bne", "bge", "blt"};
 vector<string> U_Format = {"auipc", "lui"};
 vector<string> UJ_Format = {"jal"};
+
+void trimStrings(vector<string>&tokens) {       //utility function to trim vector of strings 
+    for(int i=0;i<tokens.size();i++) {
+        tokens[i] = trim(tokens[i]);
+    }
+}
+
+void initialize_globals() {
+    
+    for(auto operation_code:R_Format) {
+        format_code[operation_code] = "R";
+    }
+
+    for(auto operation_code:S_Format) {
+        format_code[operation_code] = "S";
+    }
+
+    for(auto operation_code:I_Format) {
+        format_code[operation_code] = "I";
+    }
+
+    for(auto operation_code:SB_Format) {
+        format_code[operation_code] = "SB";
+    }
+
+    for(auto operation_code:U_Format) {
+        format_code[operation_code] = "U";
+    }
+
+    for(auto operation_code:UJ_Format) {
+        format_code[operation_code] = "UJ";
+    }
+
+    // initialize opcodes for respective formats
+    //R-Format
+    opcode["add"] = 51;  // 0110011
+    opcode["and"] = 51;  // 0110011
+    opcode["or"] = 51;   // 0110011
+    opcode["sll"] = 51;  // 0110011
+    opcode["slt"] = 51;  // 0110011
+    opcode["sra"] = 51;  // 0110011
+    opcode["srl"] = 51;  // 0110011
+    opcode["sub"] = 51;  // 0110011
+    opcode["xor"] = 51;  // 0110011
+    opcode["mul"] = 51;  // 0110011
+    opcode["div"] = 51;  // 0110011
+    opcode["rem"] = 51;  // 0110011
+
+    //I-Format
+    opcode["addi"] = 19;  // 0010011
+    opcode["andi"] = 19;  // 0010011
+    opcode["ori"] = 19;   // 0010011
+    opcode["lb"] = 3;     // 0000011
+    opcode["ld"] = 3;     // 0000011
+    opcode["lh"] = 3;     // 0000011
+    opcode["lw"] = 3;     // 0000011
+    opcode["jalr"] = 103; // 1100111
+
+    //S-Format
+    opcode["sb"] = 23;  // 0100011
+    opcode["sw"] = 27;  // 0100011
+    opcode["sd"] = 31;  // 0100011
+    opcode["sh"] = 25;  // 0100011
+
+
+    //SB-Format
+    opcode["beq"] = 99;  // 1100011
+    opcode["bne"] = 99;  // 1100011
+    opcode["bge"] = 99;  // 1100011
+    opcode["blt"] = 99;  // 1100011
+
+    //U-Format
+    opcode["auipc"] = 23; // 0010111
+    opcode["lui"] = 55;   // 0110111
+
+    //UJ-Format
+    opcode["jal"] = 111; // 1101111
+
+    // Func3 for R-Format Instructions
+    func3["add"] = 0;
+    func3["and"] = 7;
+    func3["or"] = 6;
+    func3["sll"] = 1;
+    func3["slt"] = 2;
+    func3["sra"] = 5;
+    func3["srl"] = 5;
+    func3["sub"] = 0;
+    func3["xor"] = 4;
+    func3["mul"] = 1;
+    func3["div"] = 4;
+    func3["rem"] = 6;
+
+    // Func7 for R-Format Instructions
+    func7["add"] = 0;
+    func7["and"] = 0;
+    func7["or"] = 0;
+    func7["sll"] = 0;
+    func7["slt"] = 0;
+    func7["sra"] = 32;
+    func7["srl"] = 0;
+    func7["sub"] = 32;
+    func7["xor"] = 0;
+    func7["mul"] = 1;
+    func7["div"] = 1;
+    func7["rem"] = 1;
+
+    //Func3 for I-Format Instructions
+    func3["addi"] = 0;
+    func3["andi"] = 7;
+    func3["ori"] = 6;
+    func3["lb"] = 0;
+    func3["ld"] = 3;
+    func3["lh"] = 1;
+    func3["lw"] = 2;
+    func3["jalr"] = 0;
+
+    //Func3 for S-Format Instructions
+    func3["sb"] = 0;
+    func3["sw"] = 2;
+    func3["sd"] = 3;
+    func3["sh"] = 1;
+
+    //Func3 for SB-Format Instructions
+    func3["beq"] = 0;
+    func3["bne"] = 1;
+    func3["bge"] = 5;
+    func3["blt"] = 4;
+
+}
+
+
+
+
 
 string trim(const string& str) { // Function to trim the leading and trailing whitespaces in string
     size_t start = str.find_first_not_of(" \t");
@@ -47,6 +181,21 @@ vector<string> splitByWhitespace(const string& input) { // Split a string by whi
         tokens.push_back(token);
     }
     return tokens;
+}
+
+vector<string>process_token(string &Inst) {
+  vector<string>tokens_comma = splitString(Inst,',');
+  vector<string> tokens;
+  vector<string> tokens1 = splitByWhitespace(tokens_comma[0]);
+  tokens.push_back(tokens1[0]);
+  tokens.push_back(tokens1[1]);
+  tokens_comma.erase(tokens_comma.begin());
+
+  for(auto token:tokens_comma) {
+    tokens.push_back(token);
+  }
+
+  return tokens;
 }
 
 string concatenateWithWhitespace(const vector<string>& vec) {
--- opcode_parser.cpp
+++ opcode_parser.cpp
@@ -2,37 +2,24 @@
 #include "global.hh"
 using namespace std;
 
+bool ifopened = false;
 
-void initialize_globals() {
-    
-    for(auto operation_code:R_Format) {
-        format_code[operation_code] = "R";
+void print(vector<string>&tokens){
+    if(!ifopened){
+        freopen("output.mc", "w", stdout);
+        ifopened = true;    
     }
-
-    for(auto operation_code:S_Format) {
-        format_code[operation_code] = "S";
+    for(auto token:tokens) {
+        cout << token << " ";
     }
-
-    for(auto operation_code:I_Format) {
-        format_code[operation_code] = "I";
-    }
-
-    for(auto operation_code:SB_Format) {
-        format_code[operation_code] = "SB";
-    }
-
-    for(auto operation_code:U_Format) {
-        format_code[operation_code] = "U";
-    }
-
-    for(auto operation_code:UJ_Format) {
-        format_code[operation_code] = "UJ";
-    }
+    cout << endl;
+    cout << format_code[tokens[0]] << endl;
+    cout << format_code.size() << endl;
 }
 
 
 uint32_t decodeRFormat(vector<string>&tokens) {
-
+    
 }
 
 uint32_t decodeIFormat(vector<string>&tokens) {
@@ -53,42 +40,6 @@ uint32_t decodeUFormat(vector<string>&tokens) {
 
 uint32_t decodeUJFormat(vector<string>&tokens) {
     
-}
-
-bool ifopened = false;
-
-void print(vector<string>&tokens){
-    if(!ifopened){
-        freopen("output.mc", "w", stdout);
-        ifopened = true;    
-    }
-    for(auto token:tokens) {
-        cout << token << " ";
-    }
-    cout << endl;
-    cout << format_code[tokens[0]] << endl;
-    cout << format_code.size() << endl;
-}
-
-void trimStrings(vector<string>&tokens) {       //utility function to trim vector of strings 
-    for(int i=0;i<tokens.size();i++) {
-        tokens[i] = trim(tokens[i]);
-    }
-}
-
-vector<string>process_token(string &Inst) {
-  vector<string>tokens_comma = splitString(Inst,',');
-  vector<string> tokens;
-  vector<string> tokens1 = splitByWhitespace(tokens_comma[0]);
-  tokens.push_back(tokens1[0]);
-  tokens.push_back(tokens1[1]);
-  tokens_comma.erase(tokens_comma.begin());
-
-  for(auto token:tokens_comma) {
-    tokens.push_back(token);
-  }
-
-  return tokens;
 }
 
 uint32_t process_Instruction(string& Inst, uint32_t& Instruction_Address) {
